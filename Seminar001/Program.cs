153df9f28c6e10f41248d04386fb4ea5a233e277
«KolesnikovaEV» <kibenko.ekaterina@gmail.com>
2022-07-19 13:12:57 +0300

    Решили 1 задачу на семинаре

---
+++ Seminar001/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine ("Введите number: ");
+int number = new Random(). Next(9,100);
+Console.WriteLine(number);
+int x = number/10;
+int y = number%10;
+if(x>y)
+{
+ Console.WriteLine(x);
+}
+else
+{
+    Console.WriteLine(y);
+}
