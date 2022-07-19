fb3ed08a6c8e7ca2e0bede2f4c749264c9219e7d
«KolesnikovaEV» <kibenko.ekaterina@gmail.com>
2022-07-19 15:00:36 +0300

    Решили 2 задачу на семинаре

---
+++ Seminar002/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задать массив из 8 элементов и вывести их на экран 
+
+char[] CreateArray(int count)
+{
+    return new char[count];
+}
+
+void Print(char[] array)
+{
+    int len = array.Length;
+    int i = 0;
+    while(i<len)
+    {
+        Console.WriteLine(array[i]);
+        i++;
+    }
+}
+
+void Fill(char[] array)
+{
+    int length = array.Length;
+    int i = 0;
+    while(i<length)
+    {
+        array[i++] = Convert.ToChar(new Random(). Next(64, 64+22));
+
+    }
+}
+
+
+char[] arg = CreateArray(8);
+Fill (arg);
+Print(arg);
