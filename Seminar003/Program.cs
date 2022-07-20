088e2f173b467b92359cf91088de809c2d1bb686
«KolesnikovaEV» <kibenko.ekaterina@gmail.com>
2022-07-20 23:15:35 +0300

    Решили задачу с координатами

---
+++ Seminar003/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine($"Введите X: ");
+double x = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine($"Введите y: ");
+double y = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine($"P: ({x}; {y})");
+if(x>0 && y>0) {Console.WriteLine($"1 четверть");}
+if(x<0 && y>0) {Console.WriteLine($"2 четверть");}
+if(x<0 && y<0) {Console.WriteLine($"3 четверть");}
+if(x>0 && y<0) {Console.WriteLine($"4 четверть");}
